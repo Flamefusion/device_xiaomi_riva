3fbcde34575e8cf3a4c5c6e73d9869e378ba0130
Ahmad Rulim <mfalkia342@gmail.com>
2020-04-19 12:26:38 +0700

    riva: parts: Add DiracSound

--- parts/Android.mk
+++ parts/Android.mk
@@ -15,6 +15,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_USE_AAPT2 := true
 
@@ -27,5 +28,7 @@ LOCAL_RESOURCE_DIR := \
     $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
